a2d38a90371e0f4633fbb0e2dc4cf40c1e13c5c2
Yoldaş Ulaş <yutalas@gmail.com>
2023-07-12 02:24:08 +0300

    Update Car.cs

--- Assets/Art/Assets/Scripts/Demo/Car.cs
+++ Assets/Art/Assets/Scripts/Demo/Car.cs
@@ -1,25 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Car : PortalTraveller {
+public class Car : PortalTraveller
+{
     public float maxSpeed = 1;
-    float speed;
-    float targetSpeed;
-    float smoothV;
+    private float smoothV;
+    private float speed;
+    private float targetSpeed;
 
-    void Start () {
-        Debug.Log ("Press C to stop/start car");
+    private void Start()
+    {
+        Debug.Log("Press C to stop/start car");
         targetSpeed = maxSpeed;
     }
 
-    void Update () {
-        float moveDst = Time.deltaTime * speed;
+    private void Update()
+    {
+        var moveDst = Time.deltaTime * speed;
         transform.position += transform.forward * Time.deltaTime * speed;
 
-        if (Input.GetKeyDown (KeyCode.C)) {
-            targetSpeed = (targetSpeed == 0) ? maxSpeed : 0;
-        }
-        speed = Mathf.SmoothDamp (speed, targetSpeed, ref smoothV, .5f);
+        if (Input.GetKeyDown(KeyCode.C)) targetSpeed = targetSpeed == 0 ? maxSpeed : 0;
+        speed = Mathf.SmoothDamp(speed, targetSpeed, ref smoothV, .5f);
     }
 }
