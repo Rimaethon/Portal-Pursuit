6adbe1fa98139b0a0d856ce86ebb79046502a6be
Yoldaş Ulaş <yutalas@gmail.com>
2023-07-12 02:25:52 +0300

    Update GoalPickup.cs

--- Assets/Scripts/Pickups/GoalPickup.cs
+++ Assets/Scripts/Pickups/GoalPickup.cs
@@ -1,25 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 /// <summary>
-/// Class for a pickup which marks the goal as complete
+///     Class for a pickup which marks the goal as complete
 /// </summary>
 public class GoalPickup : Pickup
 {
     /// <summary>
-    /// Description:
-    /// When picked up, marks the goal as complete
-    /// Inputs: Collider collision
-    /// Outputs: N/A
+    ///     Description:
+    ///     When picked up, marks the goal as complete
+    ///     Inputs: Collider collision
+    ///     Outputs: N/A
     /// </summary>
     /// <param name="collision">The collider that caused this to be picked up</param>
     public override void DoOnPickup(Collider collision)
     {
-        if (collision.tag == "Player")
-        {
-            GoalManager.CompleteGoal();
-        }
+        if (collision.tag == "Player") GoalManager.CompleteGoal();
         base.DoOnPickup(collision);
     }
 }
