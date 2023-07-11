94c7561c4bff833a68eaabde537e395a4b980517
Yoldaş Ulaş <yutalas@gmail.com>
2023-07-12 02:24:15 +0300

    Update CloudCoreTest.cs

--- Assets/Art/Assets/Scripts/Demo/CloudCoreTest.cs
+++ Assets/Art/Assets/Scripts/Demo/CloudCoreTest.cs
@@ -1,26 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CloudCoreTest : MonoBehaviour {
-
+public class CloudCoreTest : MonoBehaviour
+{
     public float falloffDstHorizontal = 3;
     public float falloffVertical = 1.5f;
     public float maxScale = 1;
 
-    public Vector2 rotSpeedMinMax = new Vector2 (10, 20);
+    public Vector2 rotSpeedMinMax = new(10, 20);
 
-    float rotSpeed;
+    [HideInInspector] public Transform myTransform;
 
-    [HideInInspector]
-    public Transform myTransform;
+    private float rotSpeed;
 
-    void Start () {
-        rotSpeed = Random.Range (rotSpeedMinMax.x, rotSpeedMinMax.y);
+    private void Start()
+    {
+        rotSpeed = Random.Range(rotSpeedMinMax.x, rotSpeedMinMax.y);
         myTransform = transform;
     }
 
-    void Update () {
-        myTransform.RotateAround (transform.parent.position, Vector3.up, Time.deltaTime * rotSpeed);
+    private void Update()
+    {
+        myTransform.RotateAround(transform.parent.position, Vector3.up, Time.deltaTime * rotSpeed);
     }
 }
