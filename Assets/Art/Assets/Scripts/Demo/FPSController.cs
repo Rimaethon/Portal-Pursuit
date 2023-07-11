0b2dba0088f43d778a92f6993661cf994a88632e
Yoldaş Ulaş <yutalas@gmail.com>
2023-07-12 02:24:24 +0300

    Update FPSController.cs

--- Assets/Art/Assets/Scripts/Demo/FPSController.cs
+++ Assets/Art/Assets/Scripts/Demo/FPSController.cs
@@ -1,9 +1,7 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class FPSController : PortalTraveller {
-
+public class FPSController : PortalTraveller
+{
     public float walkSpeed = 3;
     public float runSpeed = 6;
     public float smoothMoveTime = 0.1f;
@@ -12,36 +10,38 @@ public class FPSController : PortalTraveller {
 
     public bool lockCursor;
     public float mouseSensitivity = 10;
-    public Vector2 pitchMinMax = new Vector2 (-40, 85);
+    public Vector2 pitchMinMax = new(-40, 85);
     public float rotationSmoothTime = 0.1f;
-
-    CharacterController controller;
-    Camera cam;
     public float yaw;
     public float pitch;
-    float smoothYaw;
-    float smoothPitch;
+    private Camera cam;
 
-    float yawSmoothV;
-    float pitchSmoothV;
-    float verticalVelocity;
-    Vector3 velocity;
-    Vector3 smoothV;
-    Vector3 rotationSmoothVelocity;
-    Vector3 currentRotation;
+    private CharacterController controller;
+    private Vector3 currentRotation;
+    private bool disabled;
 
-    bool jumping;
-    float lastGroundedTime;
-    bool disabled;
+    private bool jumping;
+    private float lastGroundedTime;
+    private float pitchSmoothV;
+    private Vector3 rotationSmoothVelocity;
+    private float smoothPitch;
+    private Vector3 smoothV;
+    private float smoothYaw;
+    private Vector3 velocity;
+    private float verticalVelocity;
 
-    void Start () {
+    private float yawSmoothV;
+
+    private void Start()
+    {
         cam = Camera.main;
-        if (lockCursor) {
+        if (lockCursor)
+        {
             Cursor.lockState = CursorLockMode.Locked;
             Cursor.visible = false;
         }
 
-        controller = GetComponent<CharacterController> ();
+        controller = GetComponent<CharacterController>();
 
         yaw = transform.eulerAngles.y;
         pitch = cam.transform.localEulerAngles.x;
@@ -49,79 +49,84 @@ public class FPSController : PortalTraveller {
         smoothPitch = pitch;
     }
 
-    void Update () {
-        if (Input.GetKeyDown (KeyCode.P)) {
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
-            Debug.Break ();
+            Debug.Break();
         }
-        if (Input.GetKeyDown (KeyCode.O)) {
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
             disabled = !disabled;
         }
 
-        if (disabled) {
-            return;
-        }
+        if (disabled) return;
 
-        Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
+        var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
 
-        Vector3 inputDir = new Vector3 (input.x, 0, input.y).normalized;
-        Vector3 worldInputDir = transform.TransformDirection (inputDir);
+        var inputDir = new Vector3(input.x, 0, input.y).normalized;
+        var worldInputDir = transform.TransformDirection(inputDir);
 
-        float currentSpeed = (Input.GetKey (KeyCode.LeftShift)) ? runSpeed : walkSpeed;
-        Vector3 targetVelocity = worldInputDir * currentSpeed;
-        velocity = Vector3.SmoothDamp (velocity, targetVelocity, ref smoothV, smoothMoveTime);
+        var currentSpeed = Input.GetKey(KeyCode.LeftShift) ? runSpeed : walkSpeed;
+        var targetVelocity = worldInputDir * currentSpeed;
+        velocity = Vector3.SmoothDamp(velocity, targetVelocity, ref smoothV, smoothMoveTime);
 
         verticalVelocity -= gravity * Time.deltaTime;
-        velocity = new Vector3 (velocity.x, verticalVelocity, velocity.z);
+        velocity = new Vector3(velocity.x, verticalVelocity, velocity.z);
 
-        var flags = controller.Move (velocity * Time.deltaTime);
-        if (flags == CollisionFlags.Below) {
+        var flags = controller.Move(velocity * Time.deltaTime);
+        if (flags == CollisionFlags.Below)
+        {
             jumping = false;
             lastGroundedTime = Time.time;
             verticalVelocity = 0;
         }
 
-        if (Input.GetKeyDown (KeyCode.Space)) {
-            float timeSinceLastTouchedGround = Time.time - lastGroundedTime;
-            if (controller.isGrounded || (!jumping && timeSinceLastTouchedGround < 0.15f)) {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var timeSinceLastTouchedGround = Time.time - lastGroundedTime;
+            if (controller.isGrounded || (!jumping && timeSinceLastTouchedGround < 0.15f))
+            {
                 jumping = true;
                 verticalVelocity = jumpForce;
             }
         }
 
-        float mX = Input.GetAxisRaw ("Mouse X");
-        float mY = Input.GetAxisRaw ("Mouse Y");
+        var mX = Input.GetAxisRaw("Mouse X");
+        var mY = Input.GetAxisRaw("Mouse Y");
 
         // Verrrrrry gross hack to stop camera swinging down at start
-        float mMag = Mathf.Sqrt (mX * mX + mY * mY);
-        if (mMag > 5) {
+        var mMag = Mathf.Sqrt(mX * mX + mY * mY);
+        if (mMag > 5)
+        {
             mX = 0;
             mY = 0;
         }
 
         yaw += mX * mouseSensitivity;
         pitch -= mY * mouseSensitivity;
-        pitch = Mathf.Clamp (pitch, pitchMinMax.x, pitchMinMax.y);
-        smoothPitch = Mathf.SmoothDampAngle (smoothPitch, pitch, ref pitchSmoothV, rotationSmoothTime);
-        smoothYaw = Mathf.SmoothDampAngle (smoothYaw, yaw, ref yawSmoothV, rotationSmoothTime);
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        smoothPitch = Mathf.SmoothDampAngle(smoothPitch, pitch, ref pitchSmoothV, rotationSmoothTime);
+        smoothYaw = Mathf.SmoothDampAngle(smoothYaw, yaw, ref yawSmoothV, rotationSmoothTime);
 
         transform.eulerAngles = Vector3.up * smoothYaw;
         cam.transform.localEulerAngles = Vector3.right * smoothPitch;
-
     }
 
-    public override void Teleport (Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot) {
+    public override void Teleport(Transform fromPortal, Transform toPortal, Vector3 pos, Quaternion rot)
+    {
         transform.position = pos;
-        Vector3 eulerRot = rot.eulerAngles;
-        float delta = Mathf.DeltaAngle (smoothYaw, eulerRot.y);
+        var eulerRot = rot.eulerAngles;
+        var delta = Mathf.DeltaAngle(smoothYaw, eulerRot.y);
         yaw += delta;
         smoothYaw += delta;
         transform.eulerAngles = Vector3.up * smoothYaw;
-        velocity = toPortal.TransformVector (fromPortal.InverseTransformVector (velocity));
-        Physics.SyncTransforms ();
+        velocity = toPortal.TransformVector(fromPortal.InverseTransformVector(velocity));
+        Physics.SyncTransforms();
     }
-
 }
