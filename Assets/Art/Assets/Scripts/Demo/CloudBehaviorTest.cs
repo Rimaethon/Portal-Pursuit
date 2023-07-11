b453bd86a5b0dec9cd2e9b4bd2706ceb3c9b93b6
Yoldaş Ulaş <yutalas@gmail.com>
2023-07-12 02:24:12 +0300

    Update CloudBehaviorTest.cs

--- Assets/Art/Assets/Scripts/Demo/CloudBehaviorTest.cs
+++ Assets/Art/Assets/Scripts/Demo/CloudBehaviorTest.cs
@@ -1,34 +1,38 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CloudBehaviorTest : MonoBehaviour {
-    public Vector2 rotSpeedMinMax = new Vector2 (10, 20);
+public class CloudBehaviorTest : MonoBehaviour
+{
+    public Vector2 rotSpeedMinMax = new(10, 20);
 
-    float rotSpeed;
+    private CloudCoreTest[] cloudCentres;
+    private Transform myTransform;
 
-    CloudCoreTest[] cloudCentres;
-    Transform myTransform;
+    private float rotSpeed;
 
-    void Start () {
-        rotSpeed = Random.Range (rotSpeedMinMax.x, rotSpeedMinMax.y);
-        cloudCentres = FindObjectsOfType<CloudCoreTest> ();
+    private void Start()
+    {
+        rotSpeed = Random.Range(rotSpeedMinMax.x, rotSpeedMinMax.y);
+        cloudCentres = FindObjectsOfType<CloudCoreTest>();
         myTransform = transform;
     }
 
-    void Update () {
-        myTransform.RotateAround (transform.parent.position, Vector3.up, Time.deltaTime * rotSpeed);
+    private void Update()
+    {
+        myTransform.RotateAround(transform.parent.position, Vector3.up, Time.deltaTime * rotSpeed);
         float maxScale = 0;
-        for (int i = 0; i < cloudCentres.Length; i++) {
-            CloudCoreTest cloudCentre = cloudCentres[i];
-            Vector3 offset = (myTransform.position - cloudCentre.transform.position);
-            float sqrDstHorizontal = offset.x * offset.x + offset.z * offset.z;
-            float sqrDstVertical = offset.y * offset.y;
-            float tH = 1 - Mathf.Min (1, sqrDstHorizontal / (cloudCentre.falloffDstHorizontal * cloudCentre.falloffDstHorizontal));
-            float tV = 1 - Mathf.Min (1, sqrDstVertical / (cloudCentre.falloffVertical * cloudCentre.falloffVertical));
+        for (var i = 0; i < cloudCentres.Length; i++)
+        {
+            var cloudCentre = cloudCentres[i];
+            var offset = myTransform.position - cloudCentre.transform.position;
+            var sqrDstHorizontal = offset.x * offset.x + offset.z * offset.z;
+            var sqrDstVertical = offset.y * offset.y;
+            var tH = 1 - Mathf.Min(1,
+                sqrDstHorizontal / (cloudCentre.falloffDstHorizontal * cloudCentre.falloffDstHorizontal));
+            var tV = 1 - Mathf.Min(1, sqrDstVertical / (cloudCentre.falloffVertical * cloudCentre.falloffVertical));
             //float t = 1 - Mathf.Min (1, sqrDst / (falloffDst * falloffDst));
-            maxScale = Mathf.Max (maxScale, tV * tH * cloudCentre.maxScale);
+            maxScale = Mathf.Max(maxScale, tV * tH * cloudCentre.maxScale);
         }
+
         myTransform.localScale = Vector3.one * maxScale;
     }
 }
